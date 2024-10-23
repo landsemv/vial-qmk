8c057a20eb8bdb6f1364529d91431a9c58d2cf3d
landsemv <landsem.v@gmail.com>
2024-10-23 22:38:20 +0200

    New KB

---
+++ keyboards/nyx/halconf.h
@@ -0,0 +1,6 @@
+#pragma  once
+
+#define HAL_USE_SERIAL TRUE     // Enable ChibiOS Serial feature
+#define HAL_USE_PWM TRUE        // Enable ChibiOS PWM feature
+
+#include_next <halconf.h>
---
+++ keyboards/nyx/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x44, 0x12, 0xFE, 0x8D, 0x8D, 0x9E, 0x5A, 0xCA}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 0}
